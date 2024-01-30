bbb1961dd9c17c3bc561580264f9a610758ee74b
klld <kll.d.repl.co@hotmail.com>
2024-01-30 15:18:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import asyncio
 import os
 import sys
 
-if not os.getenv('DEVICE_ID') and \
-        not os.getenv('ACCOUNT_ID') and \
-        not os.getenv('SECRET'):
+if not os.environ.get('DEVICE_ID') and \
+        not os.environ.get('ACCOUNT_ID') and \
+        not os.environ.get('SECRET'):
     print("Please paste your device auths into the \"env\" file.\n")
     sys.exit()
 
@@ -16,9 +16,9 @@ os.system('clear')
 import klldFN
 
 client = klldFN.klldFN(
-  device_id=os.getenv['DEVICE_ID'],  
-  account_id=os.getenv['ACCOUNT_ID'],
-  secret=os.getenv['SECRET']
+  device_id=os.environ.get['DEVICE_ID'],  
+  account_id=os.environ.get['ACCOUNT_ID'],
+  secret=os.environ.get['SECRET']
 )
 
 
